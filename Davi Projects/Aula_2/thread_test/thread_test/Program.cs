d4c454b7e250387bf5d8dc6c0ddfb38d79ebc57d
Hoffmann365 <129854293+Hoffmann365@users.noreply.github.com>
2024-08-05 14:05:27 -0300

    Update Program.cs
    coloquei os comentários no código do thread_test

--- Davi Projects/Aula_2/thread_test/thread_test/Program.cs
+++ Davi Projects/Aula_2/thread_test/thread_test/Program.cs
@@ -7,22 +7,30 @@ namespace thread_test
     {
         public static void Main(string[] args)
         {
+            // Cria uma nova thread que executa o método PrintNumbers;
             Thread thread = new Thread(PrintNumbers);
-            
+
+            // Inicia a thread
             thread.Start();
 
+            // Método executado na thread principal
+            PrintNumbers();
+
+            // Aguarda a conclusão da thread
             thread.Join();
             
             Console.WriteLine("Fim do programa");
         }
-
+        // Método que será executado pela thread
         static void PrintNumbers()
         {
+            // Obtém o identificador da thread atual
             string threadId = Thread.CurrentThread.ManagedThreadId.ToString();
 
             for (int i = 0; i <=5; i++)
             {
                 Console.WriteLine($"Thread {threadId} - Número {i}");
+                // Pausa de 1 segundo entre cada número
                 Thread.Sleep(1000);
             }
         }
