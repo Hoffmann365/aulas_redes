d5022761ab38ccab737e6275d1d773057edb8c50
Davi Hoffmann <daviwhagner@gmail.com>
2024-08-06 10:24:38 -0300

    aula 06/08 - MultiThread Client e Server

---
+++ Davi Projects/Aula_2/MultiThreadClient/MultiThreadClient/MultiThreadClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        try
+        {
+            string server = "192.168.88.254";
+            int port = 13000;
+
+            TcpClient client = new TcpClient(server, port);
+            NetworkStream stream = client.GetStream();
+            int i = 0;
+            while (true)
+            {
+                i++;
+                if (i == 1)
+                {
+                    Console.WriteLine("Digite seu nome:");
+                }
+                else
+                {
+                    Console.WriteLine("Digite uma mensagem para enviar ao servidor:");
+                }
+                
+                string msg = Console.ReadLine();
+                string message = msg;
+                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Enviado: {0}", message);
+
+                data = new byte[256];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string responseData = Encoding.ASCII.GetString(data, 0, bytesRead);
+                Console.WriteLine("Recebido: {0}", responseData);
+
+                if (msg == "")
+                {
+                    break;
+                }
+            }
+            
+
+            stream.Close();
+            client.Close();
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.WriteLine("\nPressione Enter para continuar...");
+        Console.Read();
+    }
+}
--- Davi Projects/Aula_2/thread_test/thread_test/Program.cs
+++ Davi Projects/Aula_2/thread_test/thread_test/Program.cs
@@ -8,31 +8,33 @@ namespace thread_test
         public static void Main(string[] args)
         {
             // Cria uma nova thread que executa o método PrintNumbers;
-            Thread thread = new Thread(PrintNumbers);
-
+            Thread thread = new Thread(() => PrintNumbers(1,5,1000));
+            Thread thread2 = new Thread(() => PrintNumbers(1,10,500));
             // Inicia a thread
             thread.Start();
-
-            // Método executado na thread principal
-            PrintNumbers();
-
+            //Inicia a thread2
+            thread2.Start();
+           
             // Aguarda a conclusão da thread
             thread.Join();
+            // Aguarda a conclusão da thread2
+            thread2.Join();
             
             Console.WriteLine("Fim do programa");
         }
         // Método que será executado pela thread
-        static void PrintNumbers()
+        static void PrintNumbers(int x, int y, int z)
         {
             // Obtém o identificador da thread atual
             string threadId = Thread.CurrentThread.ManagedThreadId.ToString();
 
-            for (int i = 0; i <=5; i++)
+            for (int i = x; i <=y; i++)
             {
                 Console.WriteLine($"Thread {threadId} - Número {i}");
                 // Pausa de 1 segundo entre cada número
-                Thread.Sleep(1000);
+                Thread.Sleep(z);
             }
         }
+        
     }
 }
